b93680955973c73c8e4766dce6d04099a59e1f77
Nick Ng <ngyewch@gmail.com>
2024-11-29 12:16:27 +0800

    feat: added version flag

--- main.go
+++ main.go
@@ -2,22 +2,55 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/ngyewch/mdbook-asciidoc/renderer"
 	"github.com/ngyewch/mdbook-plugin"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var (
-	app = &cli.App{
-		Name:   "mdbook-asciidoc",
-		Usage:  "mdbook-asciidoc",
-		Action: doMain,
-	}
+	version         string
+	commit          string
+	commitTimestamp string
 )
 
 func main() {
+	app := &cli.App{
+		Name:    "mdbook-asciidoc",
+		Usage:   "mdbook-asciidoc",
+		Version: version,
+		Action:  doMain,
+	}
+
+	cli.VersionPrinter = func(cCtx *cli.Context) {
+		var parts []string
+		if version != "" {
+			parts = append(parts, fmt.Sprintf("version=%s", version))
+		}
+		if commit != "" {
+			parts = append(parts, fmt.Sprintf("commit=%s", commit))
+		}
+		if commitTimestamp != "" {
+			formattedCommitTimestamp := func(commitTimestamp string) string {
+				epochSeconds, err := strconv.ParseInt(commitTimestamp, 10, 64)
+				if err != nil {
+					return ""
+				}
+				t := time.Unix(epochSeconds, 0)
+				return t.Format(time.RFC3339)
+			}(commitTimestamp)
+			if formattedCommitTimestamp != "" {
+				parts = append(parts, fmt.Sprintf("commitTimestamp=%s", formattedCommitTimestamp))
+			}
+		}
+		fmt.Println(strings.Join(parts, " "))
+	}
+
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
