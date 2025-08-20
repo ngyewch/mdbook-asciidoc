25240b3e139a109ec8a0a195a20a2a064b733367
Nick Ng <ngyewch@gmail.com>
2025-08-20 19:28:27 +0800

    refactor: switched to urfave/cli v3

--- go.mod
+++ go.mod
@@ -4,12 +4,6 @@ go 1.23
 
 require (
 	github.com/ngyewch/mdbook-plugin v0.1.0
-	github.com/urfave/cli/v2 v2.27.7
+	github.com/urfave/cli/v3 v3.4.1
 	github.com/yuin/goldmark v1.7.13
-)
-
-require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 )
--- main.go
+++ main.go
@@ -1,64 +1,35 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/ngyewch/mdbook-asciidoc/renderer"
 	"github.com/ngyewch/mdbook-plugin"
-	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v3"
 )
 
 var (
-	version         string
-	commit          string
-	commitTimestamp string
+	version string
 )
 
 func main() {
-	app := &cli.App{
+	app := &cli.Command{
 		Name:    "mdbook-asciidoc",
 		Usage:   "mdbook-asciidoc",
 		Version: version,
 		Action:  doMain,
 	}
 
-	cli.VersionPrinter = func(cCtx *cli.Context) {
-		var parts []string
-		if version != "" {
-			parts = append(parts, fmt.Sprintf("version=%s", version))
-		}
-		if commit != "" {
-			parts = append(parts, fmt.Sprintf("commit=%s", commit))
-		}
-		if commitTimestamp != "" {
-			formattedCommitTimestamp := func(commitTimestamp string) string {
-				epochSeconds, err := strconv.ParseInt(commitTimestamp, 10, 64)
-				if err != nil {
-					return ""
-				}
-				t := time.Unix(epochSeconds, 0)
-				return t.Format(time.RFC3339)
-			}(commitTimestamp)
-			if formattedCommitTimestamp != "" {
-				parts = append(parts, fmt.Sprintf("commitTimestamp=%s", formattedCommitTimestamp))
-			}
-		}
-		fmt.Println(strings.Join(parts, " "))
-	}
-
-	err := app.Run(os.Args)
+	err := app.Run(context.Background(), os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func doMain(cCtx *cli.Context) error {
+func doMain(ctx context.Context, cmd *cli.Command) error {
 	renderContext, err := mdbook.ParseRenderContextFromReader(os.Stdin)
 	if err != nil {
 		return err
